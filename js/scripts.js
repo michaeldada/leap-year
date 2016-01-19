171f8c8ed73d98426f72d2b5e7ad0cb369f27cd2
Joseph Arnstein and Michael Dada <mail.michaeldada@gmail.com>
2016-01-19 08:46:33 -0800

    add functionality for numbers divisible by 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var leapYear = function(year) {
-  if(year % 4 === 0){
-    return true;
+  if (year % 100 === 0){
+  return false;
+  }else if(year % 4 === 0){
+  return true;
   }else{
   return false;
   }
