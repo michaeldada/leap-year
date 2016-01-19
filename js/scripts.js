a64397a97e6f9db71fce2b2b05cfd2af83d42dcc
Joseph Arnstein and Michael Dada <mail.michaeldada@gmail.com>
2016-01-19 09:08:30 -0800

    add user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 var leapYear = function(year) {
   return((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0));
 };
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
